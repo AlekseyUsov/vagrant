9516909dca3e14d53d38b9903e875a917a8cefcd
Aleksey Usov <beckett@protonmail.ch>
2017-09-06 17:52:17 +0300

    no_proxy added to provisioner

--- files/proxy-provisioner.sh
+++ files/proxy-provisioner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PROFILE=/etc/profile
+
 while getopts :h:p: opt; do
   case $opt in
     h) if [[ ${OPTARG} = -* ]]; then
@@ -25,7 +27,8 @@ done
 echo ${PROXY}
 
 if [ ! -z "${PROXY}" ]; then
-  echo -e proxy="http://${PROXY}\n" >> /etc/yum.conf
-  echo -e "\nexport http_proxy=http://${PROXY}" >> /etc/profile
-  echo -e "export https_proxy=https://${PROXY}\n" >> /etc/profile
+  echo -e proxy="\nhttp://${PROXY}\n" >> /etc/yum.conf
+  echo -e "\nexport http_proxy=http://${PROXY}" >> ${PROFILE}
+  echo -e "export https_proxy=https://${PROXY}\n" >> ${PROFILE}
+  echo -e "export no_proxy=127.0.0.1,localhost\n" >> ${PROFILE}
 fi
